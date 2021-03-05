c2fe5f357b93fb1a9c7552f0980ca584c479a911
jib <jtfvandyke@gmail.com>
2021-03-05 19:12:21 +0000

    a useful commit message

---
+++ index.php
@@ -0,0 +1,52 @@
+<html>
+<head>
+	<link rel="shortcut icon" href="favi.png" />
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+	<header>
+		<title>Thing Finder Preview</title> 
+	</header>
+	<style>
+		input[type=button], input[type=submit], input[type=reset] {
+		  background-color: #4CAF50;
+		  border: none;
+		  color: white;
+		  padding: 16px 32px;
+		  text-decoration: none;
+		  margin: 4px 2px;
+		  cursor: pointer;
+		}
+		input[type=text] {
+			border: 4px solid #4CAF50;
+			border-radius: 3px;  
+			line-height: 50px;
+		}
+		p { 
+			color: white;
+		}
+		h1 {
+			color: white;
+		}
+		h3 {
+			color: white;
+		}
+		h4 {
+			color: white;
+		}
+		h2 {
+			color: black;
+		}
+		.content {
+		  max-width: 800px;
+		  margin: auto;
+		}
+	</style>
+</head> 
+
+	<t1>Thing Finder Demo Page</t1>
+	<form action="userTerminal/login.html" method="post">
+		<input type="submit" value="Preview User Terminal">
+	</form>
+	<form action="phoneApp/login.html" method="post">
+		<input type="submit" value="Preview Phone App">
+	</form>
+</html>
